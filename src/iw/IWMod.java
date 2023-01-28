e6e25cdbe5be5666a88ddad2fce4658bb91e5e8a
TheSlaylord <abevnyaev@gmail.com>
2023-01-28 21:55:39 +0300

    Version 1.0.1

--- src/iw/IWMod.java
+++ src/iw/IWMod.java
@@ -1,21 +1,34 @@
 package iw;
 
 import arc.*;
+import arc.struct.ObjectFloatMap;
+import arc.util.Log;
+import arc.util.Time;
 import mindustry.Vars;
-import mindustry.game.EventType;
+import mindustry.gen.Building;
 import mindustry.gen.Icon;
 import mindustry.mod.*;
 
-public class IWMod extends Mod {
+import static mindustry.game.EventType.*;
 
-    public IWMod() {
-        Events.on(EventType.BuildDamageEvent.class, e -> {
-            if (e.build.team == Vars.player.team() && !e.build.wasRecentlyDamaged()) {
+public class IWMod extends Mod {
+    static ObjectFloatMap<Building> toastTimers = new ObjectFloatMap<>();
+    static {
+        Events.on(BuildDamageEvent.class, e -> {
+            Log.info("building damaged");
+            if (e.build.team == Vars.player.team() && toastTimers.get(e.build, Time.time) <= Time.time) {
                 Vars.ui.hudfrag.showToast(Icon.warning, Core.bundle.format("iw.buildingdamaged", e.build.block.localizedName, e.build.tileX(), e.build.tileY()));
+                //20 seconds message cooldown per building
+                toastTimers.put(e.build, Time.time + 1200f);
             }
         });
-        Events.on(EventType.BlockDestroyEvent.class, e -> {
-            Vars.ui.hudfrag.showToast(Icon.cancel, Core.bundle.format("iw.buildingdestroyed", e.tile.build.block.localizedName, e.tile.build.tileX(), e.tile.build.tileY()));
+    }
+
+    public IWMod() {
+        Events.on(BlockDestroyEvent.class, e -> {
+            if (e.tile.build.team == Vars.player.team()) {
+                Vars.ui.hudfrag.showToast(Icon.cancel, Core.bundle.format("iw.buildingdestroyed", e.tile.build.block.localizedName, e.tile.build.tileX(), e.tile.build.tileY()));
+            }
         });
     }
 }
